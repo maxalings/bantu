a2ee4f3ac5846649157875686fbf32782f77089e
max alings <alings.max@gmail.com>
2024-11-25 16:04:35 +0800

    added request model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_25_075603) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_25_081020) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "requests", force: :cascade do |t|
+    t.string "status"
+    t.datetime "date"
+    t.float "duration"
+    t.bigint "user_id", null: false
+    t.bigint "service_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["service_id"], name: "index_requests_on_service_id"
+    t.index ["user_id"], name: "index_requests_on_user_id"
+  end
 
   create_table "services", force: :cascade do |t|
     t.string "title"
@@ -37,5 +49,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_25_075603) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "requests", "services"
+  add_foreign_key "requests", "users"
   add_foreign_key "services", "users"
 end
