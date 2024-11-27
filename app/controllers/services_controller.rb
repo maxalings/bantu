e0dd4877460df85327c87dd2f7d1388c09ef03ab
max alings <alings.max@gmail.com>
2024-11-27 17:15:16 +0800

    map margin

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -11,7 +11,7 @@ class ServicesController < ApplicationController
     @services = @services.where("price <= ?", params[:max_price]) if params[:max_price].present?
 
     # The `geocoded` scope filters only flats with coordinates
-  @markers = @services.geocoded.map do |service|
+    @markers = @services.geocoded.map do |service|
       {
         lat: service.latitude,
         lng: service.longitude
