cd7a18ee2fed54e4bcc90aab17f6e1059a6789e5
Nini S <ninashen1998@gmail.com>
2024-11-27 17:43:38 +0800

    added worker profile page

--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,5 @@ Rails.application.routes.draw do
   get "/profile/edit", to: "profiles#edit", as: :edit_profile
   patch "/profile", to: "profiles#update", as: :update_profile
 
+  resources :workers, only: [:show]
 end
